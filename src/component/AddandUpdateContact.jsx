e5a33899b7bb4226089b61fb657ef90a87f90fc7
Yash <yash123456789.yg@gmail.com>
2024-07-04 17:15:07 +0530

    Update AddandUpdateContact.jsx

--- src/component/AddandUpdateContact.jsx
+++ src/component/AddandUpdateContact.jsx
@@ -1,87 +1,83 @@
-import { Form, Formik, ErrorMessage, Field } from "formik";
+
+import { ErrorMessage, Field, Form, Formik } from "formik";
 import Model from "./Model";
 import { addDoc, collection, doc, updateDoc } from "firebase/firestore";
 import { db } from "../config/firebase";
 import { toast } from "react-toastify";
 import * as Yup from "yup";
 
-
 const contactSchemaValidation = Yup.object().shape({
-    name: Yup.string().required("Name is Required"),
-    email: Yup.string().email("Invalid Email").required("Email is Required"),
+  name: Yup.string().required("Name is Required"),
+  email: Yup.string().email("Invalid Email").required("Email is Required"),
 });
 
-const AddandUpdateContact = ({isOpen, onClose, isUpdate , contact}) => {
-
-
-    const addContact = async (contact) =>{
-        try{
-
-            const contactref = collection(db, "Contacts");
-            await addDoc(contactref, contact);
-            onClose();
-            toast.success("Contact Added Successfully");
-        }catch (error){}    
-    };
-
-    const updateContact = async (contact) =>{
-        try{
-
-            const contactref = doc(db, "Contacts");
-            await updateDoc(contactref, contact);
-            onClose();
-            toast.success("Contact Updated Successfully");
-        }catch (error){}
-    };
-
+const AddandUpdateContact = ({ isOpen, onClose, isUpdate, contact }) => {
+  const addContact = async (contact) => {
+    try {
+      const contactRef = collection(db, "contacts");
+      await addDoc(contactRef, contact);
+      onClose();
+      toast.success("Contact Added Successfully");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const updateContact = async (contact, id) => {
+    try {
+      const contactRef = doc(db, "contacts", id);
+      await updateDoc(contactRef, contact);
+      onClose();
+      toast.success("Contact Updated Successfully");
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
-    <div>      
-    <Model isOpen={isOpen} onClose={onClose}> 
+    <div>
+      <Model isOpen={isOpen} onClose={onClose}>
         <Formik
-        validationSchema={contactSchemaValidation}
-        initialValues={
-            isUpdate ? {
-                name:"contact.name",
-                email:"contact.email",
-            }
-            :{
-                name:"",
-                email:"",
-            }
-        }
-        onSubmit={(values) => {
-            
-            isUpdate ?
-            updateContact(values, contact.id):
-            addContact(values);
-
-        }}
+          validationSchema={contactSchemaValidation}
+          initialValues={
+            isUpdate
+              ? {
+                  name: contact.name,
+                  email: contact.email,
+                }
+              : {
+                  name: "",
+                  email: "",
+                }
+          }
+          onSubmit={(values) => {
+            console.log(values);
+            isUpdate ? updateContact(values, contact.id) : addContact(values);
+          }}
         >
-            <Form className="flex flex-col gap-4">
-                <div className="flex flex-col gap-1">
-                    <label htmlFor="name">Name</label>
-                    <Field name = "name" className="h-10 border" />
-                    <div className="text-red-500 text-rs">
-                        <ErrorMessage name="name" />
-                    </div>
-                </div>
-                <div className="flex flex-col gap-1">
-                    <label htmlFor="email">Email</label>
-                    <Field name = "email" className="h-10 border" />
-                    <div className="text-red-500 text-rs">
-                        <ErrorMessage name="email" />
-                    </div>
-                </div>
+          <Form className="flex flex-col gap-4">
+            <div className="flex flex-col gap-1">
+              <label htmlFor="name">Name</label>
+              <Field name="name" className="h-10 border" />
+              <div className=" text-xs text-red-500">
+                <ErrorMessage name="name" />
+              </div>
+            </div>
+            <div className="flex flex-col gap-1">
+              <label htmlFor="email">Email</label>
+              <Field name="email" className="h-10 border" />
+              <div className=" text-xs text-red-500">
+                <ErrorMessage name="email" />
+              </div>
+            </div>
 
-                <button className="bg-orange px-3 py-1.5 border self-end">
-                    { isUpdate ? "update" : "add"} contact
-                </button>
-            </Form>
+            <button className="self-end border bg-orange px-3 py-1.5">
+              {isUpdate ? "update" : "add"} contact
+            </button>
+          </Form>
         </Formik>
-    </Model>
+      </Model>
     </div>
-  )
-}
+  );
+};
 
 export default AddandUpdateContact;
