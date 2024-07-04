636cd40b24181579800b38365ff663a5d66baeda
Yash <yash123456789.yg@gmail.com>
2024-07-04 13:50:51 +0530

    change

--- src/component/Model.jsx
+++ src/component/Model.jsx
@@ -5,15 +5,15 @@ const Model = (onClose, isOpen, childern) => {
   return createPortal(
     <>
     {isOpen && (
-    <>
-    <div className="z-50 m-auto relative min-h-[200px] max-w-[80%] bg-white p-4">
+    <div  className="backdrop-blur h-screen absolute top-0 w-screen z-40">
+    <div className="grid place-items-center z-50 m-auto relative min-h-[200px] min-w-[80%] bg-white p-4">
             <div className="flex justify-end">
                 <AiOutlineClose onClick={onClose} className="text-2xl self-end "/>
             </div>
             {childern}
         </div>
-        <div onClick={onClose} className="backdrop-blur h-screen absolute top-0 w-screen z-40"/>
-    </>
+        
+    </div>
   )}
     </>,
   document.getElementById("modal-root"));
--- src/component/Notfound.jsx
+++ src/component/Notfound.jsx
@@ -1,7 +1,9 @@
 const Notfound = () => {
   return (
-    <div className="flex">
-      <img src="/images/Contact.png" />
+    <div className="flex justify-center items-center h-80vh gap-4 ">
+     <div>
+     <img src="/images/Contact.png" />
+     </div>
     <h3 className="text-white text-2xl font-semibold">Contact Not Found</h3>
     </div>
   )
