affbc6aac616ebf95fbdc645b73ab32b37956e4e
Yash <yash123456789.yg@gmail.com>
2024-07-04 16:47:24 +0530

    done

--- src/hooks/useDisclouse.jsx
+++ src/hooks/useDisclouse.jsx
@@ -1,24 +1,15 @@
 import { useState } from "react";
 
-
 const useDisclouse = () => {
+  const [isOpen, setOpen] = useState(false);
 
+  const onOpen = () => {
+    setOpen(true);
+  };
+  const onClose = () => {
+    setOpen(false);
+  };
+  return { onClose, onOpen, isOpen };
+};
 
-    const [isOpen, setOpen] = useState(false);
-
-    const onOpen = () => {
-      setOpen(true);
-    }
-  
-    const onClose = () => {
-      setOpen(false);
-    }
-
-
-
-  return {onClose, isOpen, onOpen} (
-    
-  )
-}
-
-export default useDisclouse
+export default useDisclouse;
